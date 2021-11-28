88d4e0d21f7f663e7ba4f828adf3f0b63445cb26
Carlo Trönnberg <carlo.tronnberg@hu.ibm.com>
2021-11-28 03:58:08 +0100

    refactor: 🍅 #1, US #1, Scenario #1 🔨 [Refactoring]

--- src/mineSweeper1.js
+++ src/mineSweeper1.js
@@ -1,21 +1,8 @@
 class Game {
   constructor(width, height) {
-    if (width == 1) {
-      if (height == 1) {
-        this.gameBoard = [[0]];
-      } else {
-        this.gameBoard = [[0], [0]];
-      }
-    } else {
-      if (height == 1) {
-        this.gameBoard = [[0, 0]];
-      } else {
-        this.gameBoard = [
-          [0, 0],
-          [0, 0],
-        ];
-      }
-    }
+    this.gameBoard = Array(height)
+      .fill()
+      .map(() => Array(width).fill(0));
   }
   getBoard() {
     return this.gameBoard;
