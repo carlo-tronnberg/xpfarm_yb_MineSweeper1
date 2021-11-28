d1e6a1650c16eb3596fade588a852d8857d7bc22
Carlo Trönnberg <carlo.tronnberg@hu.ibm.com>
2021-11-28 03:37:50 +0100

    test: 🍅 #1, US #1, Scenario #1 🔴 [Red]

--- test/mineSweeper1.test.js
+++ test/mineSweeper1.test.js
@@ -3,8 +3,11 @@ const { Game } = require('../src/mineSweeper1');
 describe("I want to play a game of Mine Sweeper where I'll win if I clear the board without stepping on a bomb", () => {
   describe('US1 Game Board Creation', () => {
     describe('Create the empty Game Board', () => {
-      it.each([[1, 1, [[0]]]])(
-        'When creating a Game Board of %i by %i, I should get %s',
+      it.each([
+        [1, 1, [[0]]],
+        [2, 1, [[0, 0]]],
+      ])(
+        'When creating a Game Board of %i by %i, I should get %o',
         (width, height, gameBoard) => {
           const game = new Game(width, height);
           expect(game.getBoard()).toEqual(gameBoard);
